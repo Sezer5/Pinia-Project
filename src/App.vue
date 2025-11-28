b2f4bee51b11bf5ad8b5bb584723c6aae533e723
Sezer5 <unalmissezer@gmail.com>
2025-11-28 21:26:48 +0300

    Pinia Project Begin

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+  <router-view></router-view>
+</div>
+  
+</template>
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="row">
+    <h1>Pinia</h1>
+    <hr/>
+    <div class="col-md-6">
+        <p>
+            <strong>
+                Sayı 7
+            </strong>
+        </p>
+        <button class="btn btn-primary me-3">+</button>
+        <button class="btn btn-outline-secondary">-</button>
+        <hr/>
+        <p>
+            <span>beyaz</span> renkli <span>Fiat</span>
+        </p>
+        <button class="btn btn-primary">Arabayı ve Sayacı Güncelle</button>
+        <hr/>
+        <button class="btn btn-primary">Yazılarım</button>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
